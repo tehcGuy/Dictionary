7cfd8250ccac8bec04fdf9b0f7141e7f6acb97d5
max <scraptrashscrap@gmail.com>
2021-03-05 07:20:03 +0100

    adding an explanation

--- src/com/practice/Dictionary.java
+++ src/com/practice/Dictionary.java
@@ -1,12 +1,23 @@
 package com.practice;
-
 import java.util.ArrayList;
+
+/*
+One of the principles of object-oriented programming is encapsulation. It says that one should hide the internal structure
+ of the object and prevent direct access to it.
+
+In your case this means that those who use your class should not "know" whether your class Dictionary uses ArrayList or
+Map or something other to store elements.
+That's why option 1 is better.
+
+One of benefits of such approach is that you can change the implementation of the class Dictionary whenever you wish,
+e.g. you may decide to use a tree structure instead of list, and all who use your class will not need to modify their code.
+*/
 
 public class Dictionary {
     private ArrayList<Definition> list; //when the object is initialized, it will take this field; this field will belong to the object!!!
 
     public Dictionary() {
-        this.list = new ArrayList<Definition>();
+        this.list = new ArrayList<>();
     }
 
     public void newEntry(String key, String value) {
--- src/com/practice/SampleTests.java
+++ src/com/practice/SampleTests.java
@@ -1,7 +1,6 @@
 package com.practice;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class SampleTests {
     @Test
